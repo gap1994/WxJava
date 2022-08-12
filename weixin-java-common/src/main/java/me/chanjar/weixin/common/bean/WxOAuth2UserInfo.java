21e55dec7b7b89194f6c204556cb178ce3c947f0
gap1994 <396912223@qq.com>
2022-08-12 17:24:50 +0800

    添加is_snapshotuser字段

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/bean/WxOAuth2UserInfo.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/bean/WxOAuth2UserInfo.java
@@ -59,10 +59,10 @@ public class WxOAuth2UserInfo implements Serializable {
   @SerializedName("privilege")
   private String[] privileges;
   /**
-   * 是否为快照页模式虚拟账号，值为0时是普通用户，1时是虚拟帐号
+   * is_snapshotuser  是否为快照页模式虚拟账号，值为0时是普通用户，1时是虚拟帐号
    */
   @SerializedName("is_snapshotuser")
-  private Integer snapshotuser;
+  private Integer snapshotUser;
 
   public static WxOAuth2UserInfo fromJson(String json) {
     return WxGsonBuilder.create().fromJson(json, WxOAuth2UserInfo.class);
